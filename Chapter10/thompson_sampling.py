2532970013d00e16b2c117b89be94c0b5da0d435
ShreyaTumma <shreya.tumma@gmail.com>
2024-07-04 21:44:29 -0700

    finished chapter 10 thompson sampling code

--- Chapter10/thompson_sampling.py
+++ Chapter10/thompson_sampling.py
@@ -2,6 +2,42 @@ import numpy as np
 
 # Setting conversion rates and the number of samples
 conversionRates = [0.15, 0.04, 0.13, 0.11, 0.05]
-
 N = 10000
+
+# Number of slot machines
 d = len(conversionRates)
+
+# Creating a dataset
+
+# Initialize a 2D array with zeros of size N * d - N rows and d columns
+X = np.zeros((N, d))
+for i in range(N):
+    for j in range(d):
+        if np.random.rand() < conversionRates[j]:
+            X[i][j] = 1
+
+# Making arrays to count losses and wins
+nPosReward = np.zeros(d)
+nNegReward = np.zeros(d)
+
+# Select the best slot machine and run it through beta distribution
+for i in range(N):
+    selected = 0
+    maxRandom = 0
+    for j in range(d):
+        randomBeta = np.random.beta(nPosReward[j] + 1, nNegReward[j] + 1)
+        if randomBeta > maxRandom:
+            maxRandom = randomBeta
+            selected = j
+
+    if X[i][selected] == 1:
+        nPosReward[selected] += 1
+    else:
+        nNegReward[selected] += 1
+
+# Show which slot machine is considered the best
+nSelected = nPosReward + nNegReward
+for i in range(d):
+    print('Machine number ' + str(i + 1) + ' was selected ' + str(nSelected[i]) + ' times')
+
+print('Conclusion: Best machine is machine number ' + str(np.argmax(nSelected) + 1))
